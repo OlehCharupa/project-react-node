b867e1a0c6b8e5bdcb8939fcb5d22efe2e1cd721
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-03-04 15:24:23 +0200

    добавил рауты, не уверен что правильно, ещё будут правки

---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,49 @@
+import { lazy } from 'react';
+
+export const paths = {
+    registration: '/registration',
+    login: '/login',
+    projects: '/projects',
+    // projectId: '/projects/:projectId/sprints',
+    // sprintId: '/projects/:projectId/sprints/:sprintId',
+}
+
+const routes = [
+    {
+        path: paths.registration,
+        label: 'Регистрация',
+        // component: lazy(() => import('../pages/.......')), //раскомментировать,  в импорт внести место и имя своего компонента
+        private: false,
+        restricted: true,
+    },
+    {
+        path: paths.login,
+        label: 'Вход',
+        // component: lazy(() => import('../pages/.....')), //раскомментировать,  в импорт внести место и имя своего компонента
+        private: false,
+        restricted: true,
+    },
+    {
+        path: paths.projects,
+        label: 'Проекты',
+        // component: lazy(() => import('../pages/......')), //раскомментировать,  в импорт внести место и имя своего компонента
+        private: true,
+        restricted: true,
+    },
+    // {
+    //     path: paths.projectId,
+    //     label: 'Проект',
+    //     // component: lazy(() => import('../pages/......')), //раскомментировать,  в импорт внести место и имя своего компонента
+    //     private: true,
+    //     restricted: true,
+    // },
+    // {
+    //     path: paths.sprintId,
+    //     label: 'Спринт',
+    //     // component: lazy(() => import('../pages/......')), //раскомментировать,  в импорт внести место и имя своего компонента
+    //     private: true,
+    //     restricted: true,
+    // }
+
+]
+export default routes;
