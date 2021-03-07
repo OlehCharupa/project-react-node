c094845ed96cbbaae5eb84004ba54525abb6ca84
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2021-03-07 12:22:51 +0200

    adds fe files for tasks

--- frontend/src/components/TasksList/TasksList.js
+++ frontend/src/components/TasksList/TasksList.js
@@ -1,26 +1,23 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useLocation, useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { TransitionGroup } from "react-transition-group";
 import transition from "styled-transition-group";
-import SprintsListItem from "../SprintsListItem/SprintsListItem";
-import { allSprintsSelector } from "../../redux/selectors/sprints-selectors";
-import sprintsOperations from "../../redux/operations/sprintsOperations";
+import TasksListItem from "../TasksListItem/TasksListItem";
+import { allTasksSelector } from "../../redux/selectors/tasks-selectors";
+import tasksOperations from "../../redux/operations/tasksOperations";
 
 const Ul = styled.ul`
   padding: 0;
   list-style: none;
-  @media (min-width: 768px) {
+
+  @media (min-width: 768px) and (max-width: 1279px) {
     display: flex;
     align-items: center;
     flex-wrap: wrap;
     justify-content: space-between;
 
     margin: -10px;
-  }
-  @media (min-width: 1280px) {
-    margin: -15px;
   }
 `;
 
@@ -36,9 +33,18 @@ const Li = transition.li.attrs({
     margin-bottom: 10px;
   }
   }
-  padding: 20px 20px 40px 20px;
+  @media (min-width: 1280px) {
+      &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+  display:flex;
+  align-items: baseline;
+  padding: 20px 30px;
+  }
 
-  cursor: pointer;
+  padding: 20px 20px 50px 20px;
+
+  cursor: default;
 
   background-color: #FFFFFF;
   box-shadow: 0px 6px 26px rgba(0, 5, 97, 0.1);
@@ -59,47 +65,29 @@ const Li = transition.li.attrs({
     transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1), transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
   }
 
-    @media (min-width: 768px) {
+    @media (min-width: 768px) and (max-width: 1279px) {
       width: calc((100% - 40px) / 2);
       margin: 10px;
   }
-
-    @media (min-width: 1280px) {
-      width: calc((100% - 90px) / 3);
-      margin: 15px;
-      padding: 20px 30px 40px 20px;
-  }
 `;
 
-const SprintsList = () => {
+const TasksList = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(sprintsOperations.fetchSprints());
+    dispatch(tasksOperations.fetchTasks());
   }, []);
 
-  const sprints = useSelector((state) => allSprintsSelector(state));
-  const location = useLocation();
-  const history = useHistory();
-
-  const sprintHandler = (e) => {
-    const { sprintId } = e.currentTarget.dataset;
-    if (e.target.nodeName !== "BUTTON") {
-      history.push({
-        pathname: `/${sprintId}`,
-        from: location,
-      });
-    }
-  };
+  const tasks = useSelector((state) => allTasksSelector(state));
 
   return (
     <TransitionGroup component={Ul}>
-      {sprints.map((sprint) => (
-        <Li key={sprint.id} data-sprint-id={sprint.id} onClick={sprintHandler}>
-          <SprintsListItem id={sprint.id} />
+      {tasks.map((task) => (
+        <Li key={task.id}>
+          <TasksListItem id={task.id} />
         </Li>
       ))}
     </TransitionGroup>
   );
 };
 
-export default SprintsList;
+export default TasksList;
