d449f5c1f6d2caa34fdbee8d84baf2d662ec5eb9
IC3CLD <67324553+IC3CLD@users.noreply.github.com>
2021-03-09 16:47:14 +0200

    v.1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import Container from "./Container/Container";
 import Header from "./Header/Header";
+import SprintCreator from './SprintCreator/SprintCreator.js'
 
 function App() {
   return (
     <Container>
       <Header />
       <h1>hello</h1>
+      <SprintCreator/>
     </Container>
   );
 }
