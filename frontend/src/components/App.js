5f31bd474f1922993b105e19d7e30cfb741cefc9
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2021-03-09 12:57:14 +0200

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,43 @@
-import styles from "./App.module.css";
+import React, { Suspense, useEffect } from "react";
+import { Switch, Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import routes from "../routes/routes";
+import authOperations from "../redux/operations/authOperations";
+import Loader from "./Loader/Loader";
+import PrivateRoute from "./PrivateRoute";
+import PublicRoute from "./PublicRoute";
 import Container from "./Container/Container";
 import Header from "./Header/Header";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(authOperations.getCurrentUser());
+  }, []);
+
   return (
-    <Container>
+    <div className="App">
       <Header />
-      <h1>hello</h1>
-    </Container>
+      <Container>
+        <Suspense fallback={<Loader />}>
+          <Switch>
+            {routes.map((route) =>
+              route.private ? (
+                <PrivateRoute key={route.label} {...route} />
+              ) : (
+                <PublicRoute
+                  key={route.label}
+                  {...route}
+                  restricted={route.restricted}
+                />
+              )
+            )}
+            <Redirect to="/registration"></Redirect>
+          </Switch>
+        </Suspense>
+      </Container>
+    </div>
   );
-}
+};
 
 export default App;
