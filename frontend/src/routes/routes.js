d9a64ea2d836c22f03e7e47ab16bd43f87b3f39e
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2021-03-09 12:56:48 +0200

    adds routes

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,49 +1,63 @@
-import { lazy } from 'react';
+import { lazy } from "react";
 
 export const paths = {
-    registration: '/registration',
-    login: '/login',
-    projects: '/projects',
-    // projectId: '/projects/:projectId/sprints',
-    // sprintId: '/projects/:projectId/sprints/:sprintId',
-}
+  registration: "/registration",
+  login: "/login",
+  projects: "/projects",
+  projectId: "/projects/:projectId",
+  sprintId: "/projects/:projectId/:sprintId",
+};
 
 const routes = [
-    {
-        path: paths.registration,
-        label: 'Регистрация',
-        // component: lazy(() => import('../pages/.......')), //раскомментировать,  в импорт внести место и имя своего компонента
-        private: false,
-        restricted: true,
-    },
-    {
-        path: paths.login,
-        label: 'Вход',
-        // component: lazy(() => import('../pages/.....')), //раскомментировать,  в импорт внести место и имя своего компонента
-        private: false,
-        restricted: true,
-    },
-    {
-        path: paths.projects,
-        label: 'Проекты',
-        // component: lazy(() => import('../pages/......')), //раскомментировать,  в импорт внести место и имя своего компонента
-        private: true,
-        restricted: true,
-    },
-    // {
-    //     path: paths.projectId,
-    //     label: 'Проект',
-    //     // component: lazy(() => import('../pages/......')), //раскомментировать,  в импорт внести место и имя своего компонента
-    //     private: true,
-    //     restricted: true,
-    // },
-    // {
-    //     path: paths.sprintId,
-    //     label: 'Спринт',
-    //     // component: lazy(() => import('../pages/......')), //раскомментировать,  в импорт внести место и имя своего компонента
-    //     private: true,
-    //     restricted: true,
-    // }
-
-]
+  {
+    path: paths.registration,
+    label: "Registration",
+    exact: true,
+    // component: lazy(() =>import("./Containers/Login/Login") /* webpackChunkName: "registration-page" */), //раскомментировать,  в импорт внести место и имя своего компонента
+    private: false,
+    restricted: true,
+  },
+  {
+    path: paths.login,
+    label: "Login",
+    exact: true,
+    // component: lazy(() =>import("./Containers/Login/Login") /* webpackChunkName: "login-page" */), //раскомментировать,  в импорт внести место и имя своего компонента
+    private: false,
+    restricted: true,
+  },
+  {
+    path: paths.projects,
+    label: "Projects",
+    exact: true,
+    // component: lazy(() =>import("./Containers/Login/Login") /* webpackChunkName: "projects-page" */), //раскомментировать,  в импорт внести место и имя своего компонента
+    private: true,
+    restricted: false,
+  },
+  {
+    path: paths.projectId,
+    label: "singleProject",
+    exact: false,
+    component: lazy(
+      () =>
+        import(
+          "../components/Sprints/Sprints"
+        ) /* webpackChunkName: "singleProject-page" */
+    ),
+    private: true,
+    restricted: false,
+  },
+  {
+    path: paths.sprintId,
+    label: "singleSprint",
+    exact: false,
+    component: lazy(
+      () =>
+        import(
+          "../components/Sprints/Sprints"
+        ) /* webpackChunkName: "singleSprint-page" */
+    ),
+    private: true,
+    restricted: false,
+  },
+];
 export default routes;
