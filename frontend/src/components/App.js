58a1c01c783bd492b3cd285c430fed94b11bf40e
OlehCharupa <65734895+OlehCharupa@users.noreply.github.com>
2021-03-09 12:03:23 +0200

    доработал рауты

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,27 @@
+import React, { Suspense } from "react";
 import styles from "./App.module.css";
 import Container from "./Container/Container";
 import Header from "./Header/Header";
+import routes from "../routes/routes.js"
+import { Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import PublicRoute from "./PublicRoute/PublicRoute";
 
 function App() {
   return (
     <Container>
       <Header />
-      <h1>hello</h1>
+      <Suspense fallback={`<h1>Loading</h1>`}>
+        <Switch>
+          {routes.map((route) => {
+            return route.private ? (
+              <PrivateRoute key={route.label} {...route} />
+            ) : (
+                <PublicRoute key={route.label} {...route} />
+              );
+          })}
+        </Switch>
+      </Suspense>
     </Container>
   );
 }
